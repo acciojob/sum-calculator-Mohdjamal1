c8dc5cf46cba9fbc69ff625e23c82c2fa87f97be
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-07-31 05:38:07 +0000

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let temp = 0;
+  let [count,setCount] = useState(0);
+  temp = temp+count;
+  function sum(e){
+    setCount(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" onChange={sum(e)} value={count}></input>
+        <p>Sum: {temp}</p>
     </div>
   )
 }
